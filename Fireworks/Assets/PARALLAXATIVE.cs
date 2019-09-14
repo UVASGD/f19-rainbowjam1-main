7c25ced1992ef176d81b08108a0899e13c3d3ba3
ryanakann <ryanakann@gmail.com>
2019-09-13 21:20:18 -0400

    par

---
+++ Fireworks/Assets/PARALLAXATIVE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PARALLAXATIVE : MonoBehaviour {
+    Vector3 origin;
+    float positionMultiplier;
+    Transform cam;
+
+    void Start () {
+        origin = transform.position;
+        positionMultiplier = (origin.z + 1f);
+        cam = Camera.main.transform;
+    }
+
+    void Update () {
+        transform.position = (Vector3)((Vector2)origin + (Vector2)cam.position * positionMultiplier) + Vector3.forward * origin.z;
+    }
+}
