1077c6cf43ad82fd464300a0f184e1cbfc79861d
ryanakann <ryanakann@gmail.com>
2019-09-14 14:19:32 -0400

    WHOMST

--- Fireworks/Assets/PARALLAXATIVE.cs
+++ Fireworks/Assets/PARALLAXATIVE.cs
@@ -6,29 +6,18 @@ public class PARALLAXATIVE : MonoBehaviour {
     Vector3 origin;
     float positionMultiplier;
     Transform cam;
-    
-    float width;
-    Vector3 widthVector;
-    Dictionary<Vector3, Transform> tiling;
-    Vector3 leftPos;
-    Vector3 midPos;
-    Vector3 rightPos;
+
+    Vector3 target;
+    Vector3 velRef;
 
     void Start () {
         origin = transform.position;
         positionMultiplier = 1f - 1f / (origin.z + 1f);
         cam = Camera.main.transform;
-        width = GetComponent<SpriteRenderer>().sprite.texture.width * GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
-        widthVector = Vector3.right * width;
-
-        tiling = new Dictionary<Vector3, Transform>();
-        // midPos = new Vector3(Mathf.Round())
-        // tiling.Add()
     }
 
-    void Update () {
-        transform.position = (Vector3)((Vector2)origin + (Vector2)cam.position * positionMultiplier) + Vector3.forward * origin.z;
-
-        
+    void FixedUpdate () {
+        target = (Vector3)((Vector2)origin + (Vector2)cam.position * positionMultiplier) + Vector3.forward * origin.z;
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velRef, 0.01f);
     }
 }
